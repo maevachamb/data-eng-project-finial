c6f9cb51df03e1f2a1b38315978fa49435f2b510
Bukky <78621744+xirab@users.noreply.github.com>
2022-03-06 17:10:08 +0100

    Update App.test.js

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -2,6 +2,6 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 test('renders learn react link', () => {
-  web = render(<App />);
+  const web = render(<App />);
   expect(web).toMatchSnapshot();
 });
