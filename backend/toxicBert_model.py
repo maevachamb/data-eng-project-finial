1eb197b37e5b6fe974e90f51359d23ce406b6c16
Samirath BORO N'GOBI <50295167+Tharimas@users.noreply.github.com>
2022-03-06 14:35:52 +0100

    Update toxicBert_model.py

--- backend/toxicBert_model.py
+++ backend/toxicBert_model.py
@@ -5,11 +5,6 @@ def load_model(model_name):
     return Detoxify(model_name)
 
 def predict_text_toxicity(model, text):
-    # This function do toxicity prediction on a text and return the toxicity.
-    # if ((text == '') or (text == 'None') ):
-    #     toxicity = 'empty ! Try again'
-    # else:
-    
     d = model.predict(text)
     keys_values = d.items()
     for key, value in keys_values:
