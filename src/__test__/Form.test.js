18c1d78712c3c42aaedd575eab0e16f5d0ae6191
Bukky <78621744+xirab@users.noreply.github.com>
2022-03-06 22:50:14 +0100

    Update Form.test.js

--- src/__test__/Form.test.js
+++ src/__test__/Form.test.js
@@ -1,7 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event' // testing helpers for imitating user 
+import { mount, shallow } from 'enzyme';
 import Form from '../Component/Form';
 
-test('renders correctly across screens', () => {
-    const tree = render(<Form/>);
-    expect(tree).toMatchSnapshot();
-  });
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Form', () => {
+    test('renders correctly across screens', () => {
+        const tree = render(<Form/>);
+        expect(tree).toMatchSnapshot();
+    });
+
+})
